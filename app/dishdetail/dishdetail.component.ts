2143dec7dcb623d66461540e97e102b2da9f2806
torekhan <torekhan93@mail.ru>
2017-12-19 14:05:12 +0000

    Animations and Gestures

--- app/dishdetail/dishdetail.component.ts
+++ app/dishdetail/dishdetail.component.ts
@@ -10,6 +10,13 @@ import { FavoriteService } from '../services/favorite.service';
 import { TNSFontIconService } from 'nativescript-ngx-fonticon';
 import { Toasty } from 'nativescript-toasty';
 
+import { Page } from "ui/page";
+import { Animation, AnimationDefinition } from "ui/animation";
+import { View } from "ui/core/view";
+import { SwipeGestureEventData, SwipeDirection } from "ui/gestures";
+import { Color } from 'color';
+import * as enums from "ui/enums";
+
 @Component({
   selector: 'app-dishdetail',
     moduleId: module.id,
@@ -17,6 +24,10 @@ import { Toasty } from 'nativescript-toasty';
   styleUrls: ['./dishdetail.component.css']
 })
 export class DishdetailComponent implements OnInit {
+  showComments: boolean = false;  
+  cardImage: View;
+  commentList: View;
+  cardLayout: View;
 
   dish: Dish;
   comment: Comment;
@@ -29,6 +40,7 @@ export class DishdetailComponent implements OnInit {
     private route: ActivatedRoute,
     private routerExtensions: RouterExtensions,
     private favoriteservice: FavoriteService,
+    private page: Page, 
     private fonticon: TNSFontIconService, 
     @Inject('BaseURL') private BaseURL) { }
 
@@ -60,4 +72,95 @@ export class DishdetailComponent implements OnInit {
   goBack(): void {
     this.routerExtensions.back();
   }
+
+  onSwipe(args: SwipeGestureEventData) {
+    
+    if (this.dish) {
+      this.cardImage = <View>this.page.getViewById<View>("cardImage");
+      this.cardLayout = <View>this.page.getViewById<View>("cardLayout");
+      this.commentList = <View>this.page.getViewById<View>("commentList");
+
+      if (args.direction === SwipeDirection.up && !this.showComments ) {
+        this.animateUp();
+      }
+      else if (args.direction === SwipeDirection.down && this.showComments ) {
+        this.showComments = false;
+        this.animateDown();
+      }
+    }
+
+  }
+    
+  showAndHideComments() {
+    this.cardImage = <View>this.page.getViewById<View>("cardImage");
+    this.cardLayout = <View>this.page.getViewById<View>("cardLayout");
+    this.commentList = <View>this.page.getViewById<View>("commentList");
+
+    if (!this.showComments ) {
+      this.animateUp();
+    }
+    else if (this.showComments ) {
+      this.showComments = false;
+      this.animateDown();
+    }
+  }
+    
+  animateUp() {
+    let definitions = new Array<AnimationDefinition>();
+    let a1: AnimationDefinition = {
+        target: this.cardImage,
+        scale: { x: 1, y: 0 },
+        translate: { x: 0, y: -200 },
+        opacity: 0,
+        duration: 500,
+        curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a1);
+
+    let a2: AnimationDefinition = {
+        target: this.cardLayout,
+        backgroundColor: new Color("#ffc107"),
+        duration: 500,
+        curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a2);
+
+    let animationSet = new Animation(definitions);
+
+    animationSet.play().then(() => {
+      this.showComments = true;
+    })
+    .catch((e) => {
+        console.log(e.message);
+    });
+  } 
+    
+  animateDown() {
+    let definitions = new Array<AnimationDefinition>();
+    let a1: AnimationDefinition = {
+        target: this.cardImage,
+        scale: { x: 1, y: 1 },
+        translate: { x: 0, y: 0 },
+        opacity: 1,
+        duration: 500,
+        curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a1);
+
+    let a2: AnimationDefinition = {
+        target: this.cardLayout,
+        backgroundColor: new Color("#ffffff"),
+        duration: 500,
+        curve: enums.AnimationCurve.easeIn
+    };
+    definitions.push(a2);
+
+    let animationSet = new Animation(definitions);
+
+    animationSet.play().then(() => {
+    })
+    .catch((e) => {
+        console.log(e.message);
+    });
+  } 
 }
--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, Inject, ChangeDetectorRef } from '@angular/core';
 
+import { TNSFontIconService } from 'nativescript-ngx-fonticon';
+import { Page } from "ui/page";
+import { View } from "ui/core/view";
+import { SwipeGestureEventData, SwipeDirection } from "ui/gestures";
+import * as enums from "ui/enums";
+
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Promotion } from '../shared/promotion';
@@ -10,11 +16,17 @@ import { DrawerPage } from '../shared/drawer/drawer.page';
 
 @Component({
   selector: 'app-home',
-    moduleId: module.id,
+  moduleId: module.id,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent extends DrawerPage implements OnInit {
+  showLeftCard: boolean = true;
+  showMiddleCard: boolean = false;
+  showRightCard: boolean = false;
+  leftCard: View;
+  middleCard: View;
+  rightCard: View;
 
   dish: Dish;
   promotion: Promotion;
@@ -27,6 +39,8 @@ export class HomeComponent extends DrawerPage implements OnInit {
     private promotionservice: PromotionService,
     private leaderservice: LeaderService,
     private changeDetectorRef:ChangeDetectorRef,
+    private page: Page,
+    private fonticon: TNSFontIconService,
     @Inject('BaseURL') private BaseURL) {
       super(changeDetectorRef);
     }
@@ -43,4 +57,163 @@ export class HomeComponent extends DrawerPage implements OnInit {
         errmess => this.leaderErrMess = <any>errmess );
   }
 
+  onSwipe(args: SwipeGestureEventData) {
+    console.log("Swipe Direction: " + args.direction);
+
+      if (args.direction === SwipeDirection.left) {
+        this.animateLeft();
+      }
+      else if (args.direction === SwipeDirection.right) {
+        this.animateRight();
+      }
+  }
+
+  animateLeft() { 
+
+    if (this.dish && this.promotion && this.leader) {
+      this.leftCard = this.page.getViewById<View>('leftCard');
+      this.middleCard  = this.page.getViewById<View>('middleCard');
+      this.rightCard  = this.page.getViewById<View>('rightCard');
+
+      if( this.showLeftCard ) {
+        this.rightCard.animate({
+          translate: { x: 2000, y: 0 }
+        })
+        .then(() => {
+          this.leftCard.animate({
+            translate: { x: -2000, y: 0 },
+            duration: 500,
+            curve: enums.AnimationCurve.easeInOut
+          })
+          .then(() => {
+            this.showLeftCard = false;
+            this.showMiddleCard = true;
+            this.middleCard.animate({
+              translate: { x: 0, y: 0 },
+              duration: 500,
+              curve: enums.AnimationCurve.easeInOut
+            });
+          });
+        });
+      }
+      else if( this.showMiddleCard ) {
+        this.leftCard.animate({
+          translate: { x: 2000, y: 0 },
+          duration: 500
+        })
+        .then(() => {
+          this.middleCard.animate({
+            translate: { x: -2000, y: 0 },
+            duration: 500,
+            curve: enums.AnimationCurve.easeInOut
+          })
+          .then(() => {
+            this.showMiddleCard = false;
+            this.showRightCard = true;
+            this.rightCard.animate({
+              translate: { x: 0, y: 0 },
+              duration: 500,
+              curve: enums.AnimationCurve.easeInOut
+            });
+          });
+        });
+      }
+      else if( this.showRightCard ) {
+        this.middleCard.animate({
+          translate: { x: 2000, y: 0 },
+          duration: 500
+        })
+        .then(() => {
+          this.rightCard.animate({
+            translate: { x: -2000, y: 0 },
+            duration: 500,
+            curve: enums.AnimationCurve.easeInOut
+          })
+          .then(() => {
+            this.showRightCard = false;
+            this.showLeftCard = true;
+            this.leftCard.animate({
+              translate: { x: 0, y: 0 },
+              duration: 500
+            });
+          });
+        });
+      }
+    }
+  }
+
+  animateRight() {
+
+    if (this.dish && this.promotion && this.leader) {
+      this.leftCard = this.page.getViewById<View>('leftCard');
+      this.middleCard  = this.page.getViewById<View>('middleCard');
+      this.rightCard  = this.page.getViewById<View>('rightCard');
+
+      if( this.showLeftCard ) {
+        this.middleCard.animate({
+          translate: { x: -2000, y: 0 },
+          duration: 500
+        })
+        .then(() => {
+          this.leftCard.animate({
+            translate: { x: 2000, y: 0 },
+            duration: 500,
+            curve: enums.AnimationCurve.easeInOut
+          })
+          .then(() => {
+            this.showLeftCard = false;
+            this.showRightCard = true;
+            this.rightCard.animate({
+              translate: { x: 0, y: 0 },
+              duration: 500,
+              curve: enums.AnimationCurve.easeInOut
+            });
+          });
+        });
+      }
+      else if( this.showMiddleCard ) {
+        this.rightCard.animate({
+          translate: { x: -2000, y: 0 },
+          duration: 500
+        })
+        .then(() => {
+          this.middleCard.animate({
+            translate: { x: 2000, y: 0 },
+            duration: 500,
+            curve: enums.AnimationCurve.easeInOut
+          })
+          .then(() => {
+            this.showMiddleCard = false;
+            this.showLeftCard = true;
+            this.leftCard.animate({
+              translate: { x: 0, y: 0 },
+              duration: 500,
+              curve: enums.AnimationCurve.easeInOut
+            });
+          });
+        });
+      }
+      else if( this.showRightCard ) {
+        this.leftCard.animate({
+          translate: { x: -2000, y: 0 },
+          duration: 500
+        })
+        .then(() => {
+          this.rightCard.animate({
+            translate: { x: 2000, y: 0 },
+            duration: 500,
+            curve: enums.AnimationCurve.easeInOut
+          })
+          .then(() => {
+            this.showRightCard = false;
+            this.showMiddleCard = true;
+            this.middleCard.animate({
+              translate: { x: 0, y: 0 },
+              duration: 500
+            });
+          });
+        });
+      }
+    }
+  }
 }
